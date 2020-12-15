46e7999961277f5364f93ddfdc5508b4bfe02351
Garrett Cintron <grrttcintron@gmail.com>
2020-12-15 09:08:27 -0800

    added home route

--- src/middleware/signup-middleware.js
+++ src/middleware/signup-middleware.js
@@ -4,8 +4,6 @@ const bcrypt = require('bcrypt');
 const base64 = require('base-64');
 const Users = require('../models/auth-schema');
 
-
-
 async function signingUp(req, res) {
   console.log('youre hitting me');
   try {
