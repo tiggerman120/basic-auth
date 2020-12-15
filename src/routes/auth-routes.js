36e9103c3d8571dfb8c4cad21343bffa4348c09d
Garrett Cintron <grrttcintron@gmail.com>
2020-12-15 13:18:23 -0800

    xxxxxxxxx

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -9,18 +9,8 @@ const signupMiddleware = require('../middleware/signup-middleware');
 
 
 
-router.post('/signup', async (req, res) => {
-  console.log('youre hitting me');
-  try {
-    console.log(req.body);
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    console.log('1', req.body.password)
-    const user = new Users(req.body);
-    console.log('2', user);
-    const record = await user.save(req.body);
-    console.log('3', record)
-    res.status(201).json(record);
-  } catch (e) { console.log(e); res.status(403).send("Error Creating User"); }
+router.post('/signup', signupMiddleware, async (req, res) => {
+  
 });
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
